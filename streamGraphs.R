1d84ce40bfbf660fd7edbd28db9d9722ca05914b
paolotof <varati@hotmail.com>
2017-09-15 20:00:08 +0200

    included computeSmoothedStacks

--- streamGraphs.R
+++ streamGraphs.R
@@ -1,7 +1,10 @@
-# source("streamGraphs.R")
+setwd('~/Dropbox/R/tests/streamGraph/')
+# source('streamGraphs.R')
 rm(list = ls())
 
 bump <- function (a) {
+# note that these are only positive values between 0 and 1, if the range of your
+# values is not within this range you could normalize them.
   x <- 1 / (.1 + runif(1, min=0, max=1))
   y <- 2 * runif(1, min=0, max=1) - .5
   z <- 10 / (.1 + runif(1, min=0, max=1))
@@ -12,8 +15,8 @@ bump <- function (a) {
 }
 
 set.seed(09022017)
-nStreams = 20
-timePoints = 200
+nStreams = 20 # number of layers
+timePoints = 200 # number of samples per layer
 
 values = matrix(0, timePoints, nStreams)
 for (icol in 1 : nStreams)
@@ -23,10 +26,27 @@ for (icol in 1 : nStreams)
 		a <- bump(a)
 	values[, icol] <- a
 }
+# try with 'actual' data, does the smoothing work?
+dat <- read.table(file = "test.txt", header = TRUE)
+dat$item <- relevel(dat$item, ref = 'ta')
+condition <- levels(dat$cond)
+itemFixated <- levels(dat$item)
+icolumn <- 1
+# update nStreams and timePoints
+nStreams <- length(condition) * length(itemFixated)
+timePoints <- max(dat$bin)
+values = matrix(0, ncol = nStreams, nrow = timePoints)
+for (icond in condition){
+  for (item in itemFixated){
+    values[, icolumn] = dat$propFix[dat$cond == icond & dat$item == item]
+    icolumn <- icolumn + 1
+  }
+}
 
 colorPalette <- colorRampPalette(c("#aaaadd", "#555566"), space = "rgb")
 cols <- colorPalette(nStreams)
-cols <- cols[sample(nStreams)]
+# assign colours randomly
+# cols <- cols[sample(nStreams)]
 
 streamGraph <- function(yy, cols, plotTitle = "Streamgraph"){
   timePoints <- dim(yy)[1]
@@ -50,7 +70,7 @@ computeStacks <- function(values, method = 'ThemeRiver'){
 		thin2large <- sort(apply(values, 2, FUN=var),
 			decreasing = FALSE, index.return = TRUE)$ix
 		idxStreams <- c(thin2large[seq(1, length(thin2large), 2)],
-						thin2large[seq(length(thin2large), 2, -2)])
+      thin2large[seq(length(thin2large), 2, -2)])
 	}
 	yy <- matrix(0, timePoints, (nStreams * 2))
 	for (iStream in 1 : nStreams){
@@ -90,12 +110,67 @@ computeStacks <- function(values, method = 'ThemeRiver'){
 	return(yy)
 }
 
-jpeg(filename = "Rplot%03d.jpeg", width = 800, height = 800)
-par(mfrow = c(2, 2))
-streamGraph(computeStacks(values, 'zero'), cols, 'zero')
-streamGraph(computeStacks(values), cols, 'Theme river')
-streamGraph(computeStacks(values, 'minimizedWiggle'), cols, 'minimized Wiggle')
-streamGraph(computeStacks(values, 'newWiggle'), cols, 'newWiggle')
-par(mfrow = c(1,1))
-dev.off()
-# source("streamGraphs.R")
+computeSmoothedStacks <- function(values, method = 'ThemeRiver'){
+	timePoints <- dim(values)[1]
+	nStreams <- dim(values)[2]
+	if (method == "newWiggle"){
+		thin2large <- sort(apply(values, 2, FUN=var),
+			decreasing = FALSE, index.return = TRUE)$ix
+		idxStreams <- c(thin2large[seq(1, length(thin2large), 2)],
+						thin2large[seq(length(thin2large), 2, -2)])
+	}
+	yy <- matrix(0, timePoints, (nStreams * 2))
+	for (iStream in 1 : nStreams){
+    tmpVals <- predict(smooth.spline(values[, iStream]))$y
+    if (method == "newWiggle")
+			tmpVals <- predict(smooth.spline(values[, idxStreams[iStream]]))$y
+		if (iStream > 1){
+			yy[, iStream * 2 - 1] <- yy[, (iStream - 1) * 2]
+			yy[, iStream * 2] <- yy[, iStream * 2 - 1] + tmpVals
+		} else {
+			switch(method,
+				ThemeRiver = {
+          yy[, 1] <- -(1/2) * predict(smooth.spline(rowSums(values)))$y
+          yy[, 2] <- yy[, iStream * 2 - 1] + tmpVals},
+				zero = {
+					yy[, 2] <- tmpVals},
+				minimizedWiggle = {
+					baseline <- array(0, timePoints)
+					for (ipoint in 1 : timePoints) {
+					  for (jStream in 1 : nStreams) {
+						  baseline[ipoint] = baseline[ipoint] +
+						  + (nStreams - jStream - .5) * values[ipoint, jStream]
+            }
+					  baseline[ipoint] = baseline[ipoint] / nStreams
+					}
+          yy[, 1] <- -predict(smooth.spline(baseline))$y
+          yy[, 2] <- yy[, iStream * 2 - 1] + tmpVals},
+				newWiggle = {
+				    baseline <- rowSums(matrix((nStreams - 1 : nStreams - .5),
+						nrow = timePoints, ncol = nStreams, byrow = TRUE) * values)
+          yy[, 1] <- -predict(smooth.spline(baseline / nStreams))$y
+          yy[, 2] <- yy[, iStream * 2 - 1] + tmpVals},
+				{ # default
+          print(paste0(baseline, 'not recognized'))
+					print('baseline value can be zero, ThemeRiver, minimizedWiggle or newWiggle')}
+			)
+		}# end: if (iStream > 1){
+	}# end:
+	return(yy)
+}
+
+
+# par(mfrow = c(2, 2))
+# streamGraph(computeStacks(values, 'zero'), cols, 'zero')
+# streamGraph(computeStacks(values), cols, 'Theme river')
+# streamGraph(computeStacks(values, 'minimizedWiggle'), cols, 'minimized Wiggle')
+# streamGraph(computeStacks(values, 'newWiggle'), cols, 'newWiggle')
+# par(mfrow = c(1,1))
+
+# jpeg(filename = "SmoothedVsNot.jpeg", width = 800, height = 800)
+par(mfrow = c(1, 2))
+streamGraph(computeStacks(values, 'newWiggle'), cols, 'Not')
+streamGraph(computeSmoothedStacks(values, 'newWiggle'), cols, 'Smoothed')
+par(mfrow = c(1, 1))
+# dev.off()
+# source('streamGraphs.R')
