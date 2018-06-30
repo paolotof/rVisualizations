f1b23ce1018e634231d98a2b7ed9cee5519abd88
paolotof <varati@hotmail.com>
2018-06-30 21:11:23 +0200

    included full structural equation model

--- semIntro/iacobucci1.R
+++ semIntro/iacobucci1.R
@@ -134,4 +134,99 @@ semPaths(semFit, whatLabels = 'std', nCharNodes = 0, layout = ly,
 semPaths(semFit, what = "std",layout=ly, residuals = FALSE, 
   nCharNodes = 0, edge.label.cex = 1.2, sizeMan = 8)
 
+print('Full model')
+fullMdlStx <- "
+# measurement model
+quality =~ q1 + q2+ q3
+cost =~ c1 + c2 + c3
+value =~ v1 + v2 + v3
+csat =~ cs1 + cs2 + cs3
+repeaT =~ r1 + r2 + r3
+# structural model
+repeaT ~ csat 
+value ~ quality 
+csat ~ quality
+value ~ cost
+repeaT ~ cost
+csat ~ value 
+"
+print("Fitting full model")
+completeModel <- sem(fullMdlStx, sample.cov=costumerQuality, sample.nobs=n)
+iacobucciOutput(completeModel)
+print("full model path estimates")
+estVals <- parameterEstimates(completeModel, standardized = TRUE)
+xtab2 <- xtable(data.frame(b = estVals[estVals$op == '~', 'rhs'], 
+	rel = "->",
+	e = estVals[estVals$op == '~', 'lhs'],
+	vals = paste0(format(estVals[estVals$op == '~', 'est'], digits = 1), 
+	ifelse(estVals[estVals$op == '~', 'pvalue'] < .5, '*', ''))))
+print(xtab2, include.rownames = FALSE)
+
+print("Range of factor loadings")
+print(range(estVals[estVals$op == "=~", "std.all"]))
+print(
+	paste0(
+		if(sum(na.omit(estVals[estVals$op == "=~", "pvalue"]) < .05) 
+			!= length(na.omit(estVals[estVals$op == "=~", "pvalue"]))){
+			"NOT "}, "all factor loadings were significant"))
+
+print("plot full model")
+semPaths(completeModel, whatLabels = "std", nCharNodes = 0, edge.label.cex = .7,
+				 residuals = FALSE)
+
+print("Squared Multiple Correlation")
+summary(completeModel, rsquare=TRUE) 
+nLatentVariables = 3;
+rSquared <- inspect(completeModel, "rsquare") 
+print("for structural equations")
+rSquared[(length(rSquared) - nLatentVariables + 1) : length(rSquared)]
+print("for X-Variables")
+
+print("Check R^2 much lower than others")
+xVar <- rSquared[1:(length(rSquared) - nLatentVariables)]
+names(xVar[xVar < (mean(xVar) - (2 * sd(xVar)))])
+
+print("Modification indices")
+# add a new line to identify the inclusion of new parameters
+fullMdlStx <- paste0(fullMdlStx, "\n#DATA-DRIVEN PARAMETERS")
+mi <- modindices(completeModel)
+# keep only modification indices higher than 5
+mi <- mi[mi$mi > 5, ]
+# if there are modification indices above 5 then
+if(sum(mi$mi > 5) > 0){
+	# order the modification indices so that we can take the highest one
+	mi <- mi[order(mi$mi, decreasing=TRUE), ]
+	# include the first modification index into model syntax
+	fullMdlStx <- paste0(fullMdlStx, '\n', mi$lhs[1], mi$op[1], mi$rhs[1])
+	completeModel2 <- sem(fullMdlStx, sample.cov = costumerQuality, sample.nobs = n)
+}
+print("Compare models' fit after including mi > 5")
+fitIndexes <- c('rmsea', 'cfi', 'srmr') 
+fits <- data.frame(rmsea = .05, cfi = .97, srmr = .05)
+fits <- rbind(fits, 
+    mdl = fitMeasures(completeModel)[fitIndexes], 
+    newMdl = fitMeasures(completeModel2)[fitIndexes])
+print(fits)
+
+print("Recursively add parameters/constrains for mi > 5") 
+improveFits <- function(mdlFit, mdlStx, covarianceMatrix, nObservations){
+	mi <- modindices(mdlFit)
+	if (sum(mi$mi > 5) == 0){
+		return(mdlStx)
+	} else {
+		print("improving the model fit")
+		# order the indexes so that we can take the highest one
+		mi <- mi[order(mi$mi, decreasing = TRUE), ]
+		# paste the model improvements to the new model sintax
+		mdlStx <- paste0(mdlStx, '\n', mi$lhs[1], mi$op[1], mi$rhs[1])
+		mdlFit <- sem(mdlStx, sample.cov = covarianceMatrix, sample.nobs = nObservations)
+		improveFits(mdlFit, mdlStx, covarianceMatrix, nObservations)
+	}}
+
+newStx <- improveFits(completeModel2, fullMdlStx, costumerQuality, n)
+print(newStx)
+completeModel2 <- sem(newStx, sample.cov=costumerQuality, sample.nobs=n)
+# Do NOT forget to check how the new model looks
+semPaths(completeModel2, what = "std", residuals = FALSE, 
+    nCharNodes = 0, edge.label.cex = 1.2, sizeMan = 8)
 # source(iacobucci1.R)
